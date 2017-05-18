e346c38baa85a39dda413d78f157ec816da6298e
Unknown <definedd4@gmail.com>
2017-05-18 19:24:12 +0300

    Add ScrambledEquals utility method
    - Add new project: MathEngine.Utility
    - Add new class: EnumerableExtensons
    - Add new method (ScrambledEquals) that compares two collections ignoring their element order
    - Cover ScrambledEquals method with tests

---
+++ MathEngine/MathEngine.Utility/EnumerableExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathEngine.Utility
+{
+    public static class EnumerableExtension
+    {
+        public static bool ScrambledEquals<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
+        {
+            var cnt = new Dictionary<T, int>();
+            foreach (T s in list1)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]++;
+                }
+                else {
+                    cnt.Add(s, 1);
+                }
+            }
+            foreach (T s in list2)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]--;
+                }
+                else {
+                    return false;
+                }
+            }
+            return cnt.Values.All(c => c == 0);
+        }
+    }
+}
---
+++ MathEngine/Tests.Utility/EnumerableExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MathEngine.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Utility
+{
+    [TestClass]
+    public class EnumerableExtensionTests
+    {
+        [TestMethod]
+        public void ScrambledEquals()
+        {
+            var arr1 = new[] {1, 2, 2, 6, 3};
+            var arr2 = new[] {2, 6, 1, 3, 2};
+
+            Assert.IsTrue(arr1.ScrambledEquals(arr2));
+            Assert.IsTrue(arr2.ScrambledEquals(arr1));
+
+            var arr3 = new[] {1, 2, 3, 6};
+            var arr4 = new[] {1, 2, 2, 3, 7, 2};
+
+            Assert.IsFalse(arr2.ScrambledEquals(arr3));
+            Assert.IsFalse(arr2.ScrambledEquals(arr4));
+        }
+    }
+}
