6835d5555f79197c4096a78dbf490a814f1682c0
Unknown <definedd4@gmail.com>
2017-05-20 17:58:31 +0300

    Add sandbox project

---
+++ MathEngine/Sandbox/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathEngine.Core;
+using MathEngine.Core.StringFormat.Parse;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "") break;
+
+                var parsed = parser.Parse(input);
+
+                Console.WriteLine();
+                PrintTree(parsed, 0, "  ");
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintTree(Term term, int indent, string tab)
+        {
+            for (int i = 0; i < indent; i++)
+            {
+                Console.Write(tab);
+            }
+
+            Console.WriteLine($"{term.Tag}");
+
+            foreach (var operand in term.Operands)
+            {
+                PrintTree(operand, indent + 1, tab);
+            }
+        }
+    }
+}
