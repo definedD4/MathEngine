86c70ce879b4c1da87f1977485af63d44375c98e
Unknown <definedd4@gmail.com>
2017-05-20 14:12:53 +0300

    Add Term comparison tests

--- MathEngine/Tests.Core/TermTests.cs
+++ MathEngine/Tests.Core/TermTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using MathEngine.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decimal = MathEngine.Core.Decimal;
 
 namespace Tests.Core
 {
@@ -40,6 +41,43 @@ namespace Tests.Core
             Assert.AreEqual(term1, term2);
             Assert.AreNotEqual(term1, term3);
             Assert.AreEqual(term1, term4);
+        }
+
+        [TestMethod]
+        public void CompareTags()
+        {
+            var term1 = new Term(new TermClass("a"));
+            var term2 = new Term(new TermClass("a"));
+            var term3 = new Term(TermClass.Add);
+
+            Assert.AreEqual(term1, term2);
+            Assert.AreNotEqual(term1, term3);
+        }
+
+        [TestMethod]
+        public void CompareIntegers()
+        {
+            var term1 = new Integer(13);
+            var term2 = new Integer(13);
+            var term3 = new Integer(15);
+            var term4 = new Term(new TermClass("x"));
+
+            Assert.AreEqual(term1, term2);
+            Assert.AreNotEqual(term1, term3);
+            Assert.AreNotEqual(term1, term4);
+        }
+
+        [TestMethod]
+        public void CompareDecimals()
+        {
+            var term1 = new Decimal((decimal) 13.5);
+            var term2 = new Decimal((decimal) 13.5);
+            var term3 = new Decimal(15);
+            var term4 = new Term(new TermClass("x"));
+
+            Assert.AreEqual(term1, term2);
+            Assert.AreNotEqual(term1, term3);
+            Assert.AreNotEqual(term1, term4);
         }
     }
 }
